35758a88a7c381d57040db0cbf3ad660e2a73f2d
Crazy-Ryan <30890405+Crazy-Ryan@users.noreply.github.com>
2025-07-31 02:43:50 +0800

    change env from json to comma separated str

--- src/controller/auth/auth.py
+++ src/controller/auth/auth.py
@@ -5,9 +5,9 @@ import secrets
 
 
 def get_key_user(api_key: str) -> str | None:
-    valid_key_map = json.loads(os.getenv("API_KEY_DIGEST_TO_USER", "{}"))
+    valid_key_digiests = os.getenv("API_KEY_DIGESTS", "").split(",")
     hashed_incoming_key = hashlib.sha256(api_key.encode()).hexdigest()
-    for valid_hash, user in valid_key_map.items():
+    for valid_hash in valid_key_digiests:
         if secrets.compare_digest(hashed_incoming_key, valid_hash):
-            return user
+            return True
     return None
